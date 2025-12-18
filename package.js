cf1ee7f3147605648c6946c553b7d403f4731039
hamalilamugwagwa-source <hamalilamugwagwa@gmail.com>
2025-12-18 13:58:05 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,21 @@
+{
+  "name": "myb-books-server",
+  "version": "1.0.0",
+  "description": "Backend for M.Y.B books",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "body-parser": "^1.20.2",
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "jsonwebtoken": "^9.0.0",
+    "nodemailer": "^6.9.7",
+    "serverless-http": "^3.2.0"
+  },
+  "devDependencies": {
+    "nodemon": "^3.1.11"
+  }
+}
